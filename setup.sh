91e721561bd3b628689284545e85aad2c94ef1f4
Phoenix <153451582+3xpl017@users.noreply.github.com>
2024-01-11 17:02:49 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,4 +7,5 @@ pip3 install platform
 pip3 install datetime
 pip3 install socket
 pip3 install colorama
+echo **************************************************************************************
 echo [>] Proceso finalizado. Ingresa el comando python3 main.py para iniciar el programa.
